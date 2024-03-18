63e2b788d5abd1268e30afc1634f479b00deb490
Robert-ArchLinux <robertsolorzano510@gmail.com>
2024-03-18 13:44:32 -0700

    Created basic server outline
    -created outline for our server.js importing express and path, whiling
    adding the middleware such as handling static files located in public/
    
    -app is listneing on port 3000

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+// need to add routes
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
